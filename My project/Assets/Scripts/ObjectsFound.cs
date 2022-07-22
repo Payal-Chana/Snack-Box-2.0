170e2311ecf39f74244c3b1123893ac71cc381a3
TenishaMoodley <2105982@students.wits.ac.za>
2022-07-22 23:49:37 +0200

    finished organisation section, only the stars left to do there. also added the final drawer organised image into the last scene

---
+++ My project/Assets/Scripts/ObjectsFound.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsFound : MonoBehaviour
+{
+    public int FoundCounter = 0;
+    public int finalNumberOfObjectsFound;
+
+    private void Update()
+    {
+        if (FoundCounter == finalNumberOfObjectsFound)
+        {
+            
+        }
+    }
+}
