97f9ce1b08b1be19a7b068af23218c6c6778cd49
TenishaMoodley <2105982@students.wits.ac.za>
2022-07-23 08:56:07 +0200

    shaking animation successful

--- My project/Assets/Scripts/CameraMovement.cs
+++ My project/Assets/Scripts/CameraMovement.cs
@@ -8,10 +8,17 @@ public class CameraMovement : MonoBehaviour
     public GameObject startPos;
     public GameObject endPos;
     public float speed = 0.5f;
+
+    public Animator AnimGameObjects;
+    public Animator AnimTexts;
+    
     // Start is called before the first frame update
     void Start()
     {
         theCamera.transform.position = startPos.transform.position;
+        AnimGameObjects.SetBool("isShaking", false);
+        AnimTexts.SetBool("isShaking", false);
+        StartCoroutine("waitForAudio");
     }
 
     // Update is called once per frame
@@ -19,7 +26,6 @@ public class CameraMovement : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, endPos.transform.position, speed * Time.deltaTime);
         
-
         if (theCamera.transform.position == endPos.transform.position)
         {
             theCamera.transform.position = startPos.transform.position;
@@ -36,5 +42,17 @@ public class CameraMovement : MonoBehaviour
                 speed = 1.5f;
             }
         }
+
+        
+    }
+
+    IEnumerator waitForAudio()
+    {
+        yield return new WaitForSeconds(2f);
+        AnimGameObjects.SetBool("isShaking", true);
+        AnimTexts.SetBool("isShaking", true);
+        yield return new WaitForSeconds(8f);
+        AnimGameObjects.SetBool("isShaking", false);
+        AnimTexts.SetBool("isShaking", false);
     }
 }
